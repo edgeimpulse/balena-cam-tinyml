ab41d93bce0cce720fadb6b7c4af5897b1190457
Aurelien Lequertier <aureliusc@gmail.com>
2020-12-30 12:06:25 +0100

    Build on-device model before downloading

--- edgeimpulse-inference/app/downloadWasm.sh
+++ edgeimpulse-inference/app/downloadWasm.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-#This script downloads WASM classifier using Edge Impulse API
+#This script builds and downloads WASM classifier using Edge Impulse API
 # API Key and Project ID are defined as environment variables in BalenaCloud
 
 # Fill out and uncomment for local deployment
 #EI_API_KEY=""
 #EI_PROJECT_ID=""
+
+curl --request POST \
+  --url "https://studio.edgeimpulse.com/v1/api/$EI_PROJECT_ID/jobs/build-ondevice-model?type=wasm" \
+  --header "Accept: application/json" \
+  --header "Content-Type: application/json" \
+  --header "x-api-key: $EI_API_KEY" \
+  --data '{"engine":"tflite-eon"}'
 
 curl --request GET \
   --url "https://studio.edgeimpulse.com/v1/api/$EI_PROJECT_ID/deployment/download?type=wasm" \
