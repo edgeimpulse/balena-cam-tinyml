838ed17cfff9b80dc05fcbf01495e5006c183ad6
Aurelien Lequertier <aureliusc@gmail.com>
2021-01-11 19:23:28 +0100

    Wait until WASM build is finished before downloading zipfile

--- edgeimpulse-inference/app/downloadWasm.sh
+++ edgeimpulse-inference/app/downloadWasm.sh
@@ -6,13 +6,27 @@
 #EI_API_KEY=""
 #EI_PROJECT_ID=""
 
-curl --request POST \
+# Build WASM model and retrieves JOB ID
+JOB_ID=`curl --request POST \
   --url "https://studio.edgeimpulse.com/v1/api/$EI_PROJECT_ID/jobs/build-ondevice-model?type=wasm" \
   --header "Accept: application/json" \
   --header "Content-Type: application/json" \
   --header "x-api-key: $EI_API_KEY" \
-  --data '{"engine":"tflite-eon"}'
+  --data '{"engine":"tflite-eon"}' | sed -n '/.*id":/ { s///; s/}.*//; p; q; }'`
 
+# Loop until build is finished
+while true; do
+  BUILD_STATUS=`curl --request GET \
+    --url "https://studio.edgeimpulse.com/v1/api/$EI_PROJECT_ID/jobs/$JOB_ID/status" \
+    --header "Accept: application/json" \
+    --header "x-api-key: $EI_API_KEY" | grep "finished"`
+
+  if [ $? -ne 1 ]; then break; fi
+  echo "Still building WASM..."
+  sleep 5
+done
+
+# Retrieve WASM model
 curl --request GET \
   --url "https://studio.edgeimpulse.com/v1/api/$EI_PROJECT_ID/deployment/download?type=wasm" \
   --header "accept: application/zip" \
